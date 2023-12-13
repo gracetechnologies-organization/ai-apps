39137c2405539912268492338ec6192a69db5ac3
aashir-grace <144695368+aashir-grace@users.noreply.github.com>
2023-12-13 17:15:02 +0500

    Version1.0.3
    AWS Bedrock Model for IOS Resume

--- blueprints/iosresume_blueprint.py
+++ blueprints/iosresume_blueprint.py
@@ -1,9 +1,20 @@
 from flask import request, jsonify, Blueprint, current_app
-import openai, json, os, threading, csv
-from helpers.openaiHelper import get_openai_key_from_db
+import random, json, os, threading, csv,ast
+import boto3, botocore
+import pandas as pd
 from middlewares.resauthorization import authorization_required
+from helpers.AWSHelper import get_Creds
 
 iosresume_blueprint = Blueprint('iosresume', __name__)
+
+Creds = get_Creds()
+
+bedrock_runtime = boto3.client(
+service_name = Creds["SERVICE_NAME"],
+region_name = Creds["REGION_NAME"],
+aws_access_key_id=Creds["AWS_ACCESS_KEY"],
+aws_secret_access_key=Creds["AWS_SECRET_KEY"]
+)
 
 data_list = []
 data_lock = threading.Lock()
@@ -13,134 +24,101 @@ CSV_FILE = 'IOSDataset.csv'
 CSV_PATH = os.path.join(DATASETS_DIR, CSV_FILE)
 
 def save_to_csv(data):
-    # Check if the directory exists, if not, create it
+
     if not os.path.exists(DATASETS_DIR):
         os.makedirs(DATASETS_DIR)
 
-    # Check if the CSV file exists, if not, create it and write the header
     if not os.path.exists(CSV_PATH):
         with open(CSV_PATH, 'w', newline='') as csvfile:
             fieldnames = ['JobDescription', 'Response']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
 
-    # Append data to the CSV file
     with open(CSV_PATH, 'a', newline='') as csvfile:
         fieldnames = ['JobDescription', 'Response']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(data)
 
-openai.api_key = None
+def clean_response(txt):
+    start_index = txt.find("{")
+    end_index = txt.rfind("}") + 1
+    json_data = txt[start_index:end_index]
+    parsed_json = json.loads(json_data)
+    return parsed_json
 
-def clean_and_parse_json(input_string):
-    # Removing leading and trailing '\n' characters
-    cleaned_string = input_string.strip()
+def random_response():
+    df = pd.read_csv('Datasets/IOSDataset.csv')
+    if 'Response' in df.columns:
+        random_response = random.choice(df['Response'])
+        response_dict = ast.literal_eval(random_response)
+        json_response = json.loads(response_dict)
+        return(json_response)
+    else:
+        current_app.logger.error("The 'Response' column does not exist in the DataFrame.")
+        raise
 
-    # Removing trailing commas from the string
-    cleaned_string = cleaned_string.replace(",\n        }", "\n        }")
+def get_completion(prompt):
+    body = json.dumps({"prompt":prompt, "max_tokens_to_sample": 800})
+    modelId = "anthropic.claude-instant-v1"
+    accept = "application/json"
+    contentType = "application/json"
 
     try:
-        # Converting the string to a JSON object
-        parsed_dict = json.loads(cleaned_string)
-        return parsed_dict
-    except json.JSONDecodeError as e:
-        current_app.logger.error(f"Error occurred: {str(e)}")
-        raise InvalidResponseError("Invalid JSON format. Unable to parse.")
-class InvalidResponseError(Exception):
-    def __init__(self, message="Invalid response from AI. Unable to parse as JSON."):
-        self.message = message
-        super().__init__(self.message)
 
-def get_completion(prompt, model="gpt-3.5-turbo-instruct", api_key=None):
-    try:
-        if api_key:
-            openai.api_key = api_key
-        else:
-            raise ValueError("No API key provided.")
-
-        response = openai.Completion.create(
-            engine=model,
-            prompt=prompt,
-            max_tokens=800,  # Adjust the desired length of the generated text
-            n=1,  # Number of completions to generate
-            stop=None,  # You can specify stop sequences to end the text
-            temperature=0  # This is the degree of randomness of the model's output
+        response = bedrock_runtime.invoke_model(
+            body=body, modelId=modelId, accept=accept, contentType=contentType
         )
-        generated_text = response.choices[0].text
-        # parsed_dict = json.loads(generated_text)
-        # return parsed_dict
-        try:
-            parsed_dict = json.loads(generated_text)
-        except json.JSONDecodeError:
-            parsed_dict = clean_and_parse_json(generated_text)
+        response_body = json.loads(response.get("body").read())
+        return(clean_response(response_body.get("completion")))
 
-        return parsed_dict
-
-    except (openai.error.OpenAIError, json.JSONDecodeError, ValueError) as e:
-        current_app.logger.error(f"Error occurred: {str(e)}")
-        raise
-    except Exception as e:
-        current_app.logger.error(f"Error in get_completion function: {str(e)}")
-        raise
-
-# Define a POST endpoint for your API
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'AccessDeniedException':
+            current_app.logger.error(f"\x1b[41m{e.response['Error']['Message']}\
+                    \nTo troubeshoot this issue please refer to the following resources.\
+                    \nhttps://docs.aws.amazon.com/IAM/latest/UserGuide/troubleshoot_access-denied.html\
+                    \nhttps://docs.aws.amazon.com/bedrock/latest/userguide/security-iam.html\x1b[0m\n")
+        else:
+            raise
 @iosresume_blueprint.route('/api/ai/generate_iosresume', methods=['POST'])
 @authorization_required
 def generate_resume():
     try:
-            
-        api_key = get_openai_key_from_db()
-        
+                
         job_description = request.form.get('JobDescription')
-        # user_api_key = request.form.get('ApiKey')  # Extract user-provided API key
         
         if job_description is None:
-            raise ValueError("Job Description is missing.")
+            return jsonify({"message" : "Job Description is missing."}), 422
         elif len(job_description) < 10 or not job_description[:1].isalpha() or any(char in job_description for char in set('[~!@#$%^&*()_+{}":;\]+$')):
-            raise ValueError("Please try with correct and detailed job description having no special characters.")
+            return jsonify({"message" :"Please try with correct and detailed job description having no special characters."})
         
-        # Modify the prompt with the provided name and job_description
-        prompt = f""" Generate resume data for a candidate based on the provided job description. Include the following sections:
+        prompt = f"""Human: Generate resume data for a candidate based on the provided job description. Include the following sections:
+                    Profession
+                    Objective (250-270 characters only)
+                    Experience (List of 2 JSON objects with Designation, Organization, JoiningDate, EndDate, and short 2 line JobDescription of max 100 characters only)
+                    Qualification (List of 2 JSON objects with Degree, EducationalOrganization, Score in format (CGPA: ???/4.0), CompletionDate)
+                    Achievements (List of 4 JSON objects having "AchievementTitle" key with with short achievement )
+                    Projects (List of 4  JSON objects having only "ProjectTitle" key with short project title)
+                    Skills (List of 4 JSON objects having only "SkillName" key)
+                    Interests (List of 3 JSON object having only "Interest" key)
+                    Ensure that the all keys must be same as defined section names (starting capital letter) and format of resume data must as JSON.
+                    Job Description: "{job_description}"
+                    Assistant:
+                    """
+        parsed_dict = get_completion(prompt)
 
-                        Profession
-                        Objective (250-270 characters only)
-                        Experience (List of 2 JSON objects with Designation, Organization, JoiningDate, EndDate, and short one line JobDescription of max 100 characters only)
-                        Qualification (List of 2 JSON objects with Degree, EducationalOrganization, Score in format (CGPA: ???/4.0), CompletionDate)
-                        Achievements (List of 4 JSON objects having "AchievementTitle" key with value of max 10 characters)
-                        Projects (List of 4  JSON objects having only "ProjectTitle" key)
-                        Skills (List of 4 JSON objects having only "SkillName" key)
-                        Interests (List of 3 JSON object having only "Interest" key)
-                        Ensure that the format of resume data must as JSON.
-
-                        Job Description: "{job_description}"
-                        """
-
-        # Generate the response using the OpenAI model, passing user_api_key if provided
-        parsed_dict = get_completion(prompt, api_key=api_key)
-        
-        # Store data in a dictionary
-        data = {'JobDescription': job_description, 'Response': parsed_dict}
-
-        # Acquire lock before appending to the global list
-        with data_lock:
-            data_list.append(data)
-
-        # Offload the file writing to a separate thread
-        threading.Thread(target=save_to_csv, args=(data,)).start()
-
-        # Return the response as JSON
-        return jsonify(parsed_dict), 200
-    except json.JSONDecodeError as e:
-        current_app.logger.error(f"Error occurred:  response isn't in Json format because of job description provided: {str(e)}")
-        return jsonify({"message": "Try again with a detailed and correct job description."}), 500
-    
-    except InvalidResponseError as e:
-        current_app.logger.error(f"Error occurred: {str(e)}")
-        return jsonify({"message": "Try again with a detailed and correct job description."}), 500
+        if parsed_dict is None:
+            parsed_dict = random_response()
+            return jsonify(parsed_dict), 200
+        else:
+            data = {'JobDescription': job_description, 'Response': parsed_dict}
+            with data_lock:
+                data_list.append(data)
+            threading.Thread(target=save_to_csv, args=(data,)).start()
+            return jsonify(parsed_dict), 200
     
     except ValueError as e:
         current_app.logger.error(f"Error occurred: {str(e)}")
-        return jsonify({"message": str(e)}), 422
+        return jsonify({"message": "Try again with a detailed and correct job description."}), 422
     
     except Exception as e:
         current_app.logger.error(f"Error in generate_resume function: {str(e)}")
